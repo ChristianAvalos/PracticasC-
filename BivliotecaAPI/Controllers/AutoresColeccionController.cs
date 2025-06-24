dae8061018f08e49d27e97a43e39514f9c43c849
ChristianAvalos <christiand.avalos.e@gmail.com>
2025-06-24 11:41:51 -0300

    para las colecciones

--- BivliotecaAPI/Controllers/AutoresColeccionController.cs
+++ BivliotecaAPI/Controllers/AutoresColeccionController.cs
@@ -18,16 +18,45 @@ namespace BivliotecaAPI.Controllers
             this.context = context;
             this.mapper = mapper;
         }
+        [HttpGet("{ids}", Name = "ObtenerAutoresPorIds")]
+        public async Task<ActionResult<List<AutorConLibrosDTO>>> Get([FromRoute] string ids)
+        {
+            var idsColeccion = new List<int>();
+            foreach (var id in ids.Split(","))
+            {
+                if (int.TryParse(id, out int idInt))
+                {
+                    idsColeccion.Add(idInt);
+                }
+            } 
+            if (!idsColeccion.Any())
+            {
+                ModelState.AddModelError(nameof(ids), "Nungun Id fue encontrado");
+                return ValidationProblem();
+            }
+            var autores = await context.Autores
+                .Include(x => x.Libros)
+                .ThenInclude(x => x.Libro)
+                .Where(x => idsColeccion.Contains(x.Id))
+                .ToListAsync();
+            if (autores.Count != idsColeccion.Count)
+                {
+                return NotFound();
+                }
+            var autoresDTO = mapper.Map<IEnumerable<AutorConLibrosDTO>>(autores);
+            return Ok(autoresDTO);
+        } 
 
-       [HttpPost]
+        [HttpPost]
         public async Task<ActionResult> Post(IEnumerable<AutorCreacionDTO> autoresCreacionDTO)
         {
             var autores = mapper.Map<IEnumerable<Autor>>(autoresCreacionDTO);
             context.AddRange(autores);
             await context.SaveChangesAsync();
-            return Ok();
-            //var autoresDTO = mapper.Map<IEnumerable<AutorDTO>>(autores);
-            //return CreatedAtRoute("ObtenerAutoresColeccion", new { }, autoresDTO);
+            var autoresDTO = mapper.Map<IEnumerable<AutorDTO>>(autores);
+            var ids = autores.Select(x => x.Id);
+            var idsString = string.Join(",", ids);
+            return CreatedAtRoute("ObtenerAutoresPorIds", new { ids = idsString }, autoresDTO);
         }
         [HttpGet(Name = "ObtenerAutoresColeccion")]
         public async Task<ActionResult<IEnumerable<AutorDTO>>> Get()
